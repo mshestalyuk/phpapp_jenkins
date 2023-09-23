a68f12f3f97dcf5dfad844265f14b1d0adf0f21a
Maks <109723200+mshestalyuk@users.noreply.github.com>
2023-09-23 17:54:20 +0200

    adding 1 string / testing

--- index.php
+++ index.php
@@ -23,7 +23,7 @@ else
 </head>
 <body>
     <section class="congratulations">
-        <h1>Congratulations!</h1>
+        <h1>Congratulations! Jenkins app!</h1>
         <p>Your AWS Elastic Beanstalk <em>PHP</em> application is now running on your own dedicated environment in the AWS&nbsp;Cloud</p>
         <p>You are running PHP version <?= phpversion() ?></p>
         <p>This environment is launched with Elastic Beanstalk PHP Platform</p>
